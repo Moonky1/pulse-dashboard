e4f808a5de46b66e0506783d343d690f1485a855
Moonky1 <simon.francoau@gmail.com>
2026-03-24 11:42:17 -0500

    fix: Register.css case

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { APP_CONFIG } from '../config'
 import { validateToken } from '../utils/token'
-import './register.css'
+import './Register.css'
 
 const STEPS = ['name', 'team', 'token']
 
